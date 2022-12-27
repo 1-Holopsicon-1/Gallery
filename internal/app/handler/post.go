5094edcb9a96fb27148bc69721a7c4bd53a00038
1-Holopsicon-1 <mfnbrx@gmail.com>
2022-12-27 20:30:13 +0300

    Rework CRUD for post

--- internal/app/handler/post.go
+++ internal/app/handler/post.go
@@ -7,6 +7,7 @@ import (
 	"github.com/go-chi/chi/v5"
 	"log"
 	"net/http"
+	"strconv"
 )
 
 func (h *Handler) findPostById(w http.ResponseWriter, r *http.Request) {
@@ -19,7 +20,7 @@ func (h *Handler) findPostById(w http.ResponseWriter, r *http.Request) {
 	id := chi.URLParam(r, "id")
 	var post models.Posts
 	var user map[string]interface{}
-	fmt.Println(h.DB.Find(&post, id).Joins("join users on user_id=users.id").Scan(&user).Preload("Tags"))
+	fmt.Println(h.DB.Preload("Tags").Find(&post, id).Joins("join users on user_id=users.id").Scan(&user))
 	result := resultSt{ID: post.ID, Url: post.Url, Tags: post.Tags, UserName: user["name"].(string)}
 	fmt.Println(user)
 	json.NewEncoder(w).Encode(&result)
@@ -41,6 +42,11 @@ func (h *Handler) createPost(w http.ResponseWriter, r *http.Request) {
 		//
 		panic(err)
 	}
+	for index, value := range post.Tags {
+		var tag models.Tags
+		h.DB.FirstOrCreate(&tag, models.Tags{Name: value.Name})
+		post.Tags[index].ID = tag.ID
+	}
 	h.DB.Create(&post)
 }
 
@@ -51,9 +57,17 @@ func (h *Handler) deletePost(w http.ResponseWriter, r *http.Request) {
 
 func (h *Handler) editTags(w http.ResponseWriter, r *http.Request) {
 	var post models.Posts
+	id := chi.URLParam(r, "id")
 	err := json.NewDecoder(r.Body).Decode(&post)
 	if err != nil {
 		log.Fatalln("Error of editing tags\n", err)
 	}
-	h.DB.Save(&post)
+	for index, value := range post.Tags {
+		var tag models.Tags
+		h.DB.FirstOrCreate(&tag, models.Tags{Name: value.Name})
+		post.Tags[index].ID = tag.ID
+	}
+	convertorId, _ := strconv.ParseUint(id, 10, 64)
+	post.ID = uint(convertorId)
+	h.DB.Model(&post).Association("Tags").Replace(post.Tags)
 }
