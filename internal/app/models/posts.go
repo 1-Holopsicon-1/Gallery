b3cdf386c6f638e2f73c82ebdc2d8f31fae26b6b
1-Holopsicon-1 <mfnbrx@gmail.com>
2023-01-23 21:38:57 +0300

    Dependency Post-User

--- internal/app/models/posts.go
+++ internal/app/models/posts.go
@@ -5,4 +5,5 @@ type Posts struct {
 	Url    string  `gorm:"text" json:"url"`
 	Tags   []*Tags `gorm:"many2many:posts_tags;constraint:OnUpdate:CASCADE,OnDelete:CASCADE" json:"tags"`
 	UserId int     `json:"user_id"`
+	User   User
 }
