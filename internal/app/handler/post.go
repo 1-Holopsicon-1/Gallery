bbc0219ea8d08598e8914414c3b9a2815b6286d6
1-Holopsicon-1 <mfnbrx@gmail.com>
2023-01-23 21:36:47 +0300

    Move all logic to service

--- internal/app/handler/post.go
+++ internal/app/handler/post.go
@@ -1,73 +1,48 @@
 package handler
 
 import (
-	"Gallery/internal/app/models"
+	"Gallery/internal/app/services"
 	"encoding/json"
-	"fmt"
 	"github.com/go-chi/chi/v5"
 	"log"
 	"net/http"
-	"strconv"
 )
 
+var postService services.PostService
+
 func (h *Handler) findPostById(w http.ResponseWriter, r *http.Request) {
-	type resultSt struct {
-		ID       uint
-		Url      string
-		Tags     []*models.Tags
-		UserName string
-	}
 	id := chi.URLParam(r, "id")
-	var post models.Posts
-	var user map[string]interface{}
-	fmt.Println(h.DB.Preload("Tags").Find(&post, id).Joins("join users on user_id=users.id").Scan(&user))
-	result := resultSt{ID: post.ID, Url: post.Url, Tags: post.Tags, UserName: user["name"].(string)}
-	fmt.Println(user)
-	json.NewEncoder(w).Encode(&result)
+	err := json.NewEncoder(w).Encode(postService.FindById(h.DB, id))
+	if err != nil {
+		log.Fatalln(err)
+	}
 }
 
 func (h *Handler) getAll(w http.ResponseWriter, r *http.Request) {
-	var posts []models.Posts
-	h.DB.Find(&posts)
-	h.DB.Preload("Tags").Find(&posts)
-	if err := json.NewEncoder(w).Encode(posts); err != nil {
+	err := json.NewEncoder(w).Encode(postService.GetAll(h.DB))
+	if err != nil {
 		log.Fatalln(err)
 	}
 }
 
 func (h *Handler) createPost(w http.ResponseWriter, r *http.Request) {
-	var post models.Posts
-	err := json.NewDecoder(r.Body).Decode(&post)
+	err := json.NewEncoder(w).Encode(postService.CreatePost(h.DB, r))
 	if err != nil {
-		//
-		panic(err)
+		log.Fatalln(err)
 	}
-	for index, value := range post.Tags {
-		var tag models.Tags
-		h.DB.FirstOrCreate(&tag, models.Tags{Name: value.Name})
-		post.Tags[index].ID = tag.ID
-	}
-	h.DB.Create(&post)
 }
 
 func (h *Handler) deletePost(w http.ResponseWriter, r *http.Request) {
 	id := chi.URLParam(r, "id")
-	h.DB.Delete(&models.Posts{}, id)
+	err := json.NewEncoder(w).Encode(postService.Delete(h.DB, id))
+	if err != nil {
+		log.Fatalln(err)
+	}
 }
 
 func (h *Handler) editTags(w http.ResponseWriter, r *http.Request) {
-	var post models.Posts
-	id := chi.URLParam(r, "id")
-	err := json.NewDecoder(r.Body).Decode(&post)
+	err := json.NewEncoder(w).Encode(postService.EditTags(h.DB, r))
 	if err != nil {
-		log.Fatalln("Error of editing tags\n", err)
+		log.Fatalln(err)
 	}
-	for index, value := range post.Tags {
-		var tag models.Tags
-		h.DB.FirstOrCreate(&tag, models.Tags{Name: value.Name})
-		post.Tags[index].ID = tag.ID
-	}
-	convertorId, _ := strconv.ParseUint(id, 10, 64)
-	post.ID = uint(convertorId)
-	h.DB.Model(&post).Association("Tags").Replace(post.Tags)
 }
