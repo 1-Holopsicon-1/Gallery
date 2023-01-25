57aa8354578ba6cc141246810abb3d1560a72fb0
1-Holopsicon-1 <mfnbrx@gmail.com>
2023-01-25 23:46:46 +0300

    New method for parsing a value from a tag search

--- internal/app/utils/converter.go
+++ internal/app/utils/converter.go
@@ -33,3 +33,41 @@ func (c *Converter) PostToDto(p models.Posts) dto.PostDto {
 	pDto.User = c.UserToDtoNP(p.User)
 	return pDto
 }
+
+func (c *Converter) TransformFromSearchByTag(data []map[string]interface{}) []dto.PostDto {
+	var pDtos []dto.PostDto
+	pDto := dto.PostDto{
+		ID:  uint(data[0]["id"].(int64)),
+		Url: data[0]["url"].(string),
+		User: dto.UserDtoNP{
+			ID:   uint(data[0]["user_id"].(int64)),
+			Name: data[0]["user_name"].(string)},
+	}
+	for i, v := range data {
+		if pDto.ID == uint(v["id"].(int64)) {
+			tag := dto.TagDto{
+				ID:   uint(v["tag_id"].(int64)),
+				Name: v["tag_name"].(string)}
+			pDto.Tags = append(pDto.Tags, tag)
+		} else {
+			pDtos = append(pDtos, pDto)
+			pDto = dto.PostDto{
+				ID:   uint(v["id"].(int64)),
+				Url:  v["url"].(string),
+				Tags: []dto.TagDto{},
+				User: dto.UserDtoNP{
+					ID:   uint(v["user_id"].(int64)),
+					Name: v["user_name"].(string),
+				},
+			}
+			tag := dto.TagDto{
+				ID:   uint(v["tag_id"].(int64)),
+				Name: v["tag_name"].(string)}
+			pDto.Tags = append(pDto.Tags, tag)
+		}
+		if i == len(data)-1 {
+			pDtos = append(pDtos, pDto)
+		}
+	}
+	return pDtos
+}
