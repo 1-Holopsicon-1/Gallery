d0730694ba40b26ab9c8e13d8801e01eabda6c1f
1-Holopsicon-1 <mfnbrx@gmail.com>
2023-01-25 23:43:29 +0300

    Move and update logic of searching by tag

--- internal/app/handler/tag.go
+++ internal/app/handler/tag.go
@@ -1,18 +1,17 @@
 package handler
 
 import (
-	"Gallery/internal/app/models"
+	"Gallery/internal/app/services"
 	"encoding/json"
-	"fmt"
-	"github.com/go-chi/chi/v5"
+	"log"
 	"net/http"
 )
 
+var tagService services.TagService
+
 func (h *Handler) findByTag(w http.ResponseWriter, r *http.Request) {
-	var tag models.Tags
-	tagName := chi.URLParam(r, "tagName")
-	h.DB.Preload("Posts").Where("name ~~ ?", tagName).Find(&tag)
-	fmt.Println()
-	//TODO: Load all tags related to post
-	json.NewEncoder(w).Encode(&tag)
+	err := json.NewEncoder(w).Encode(tagService.FindPostByNameTag(h.DB, r))
+	if err != nil {
+		log.Fatalln(err)
+	}
 }
