df3c37a5c4c8aec1f07ef5444e6a4854e2c62373
1-Holopsicon-1 <mfnbrx@gmail.com>
2023-03-30 13:42:08 +0300

    CORS update

--- internal/app/handler/handler.go
+++ internal/app/handler/handler.go
@@ -3,6 +3,7 @@ package handler
 import (
 	"github.com/go-chi/chi/v5"
 	"github.com/go-chi/chi/v5/middleware"
+	"github.com/go-chi/cors"
 	"gorm.io/gorm"
 	"net/http"
 )
@@ -13,8 +14,18 @@ type Handler struct {
 
 func (h *Handler) InitRoutes() *chi.Mux {
 	router := chi.NewRouter()
+	router.Use(cors.Handler(cors.Options{
+		// AllowedOrigins:   []string{"https://foo.com"}, // Use this to allow specific origin hosts
+		AllowedOrigins: []string{"https://*", "http://*"},
+		// AllowOriginFunc:  func(r *http.Request, origin string) bool { return true },
+		AllowedMethods:   []string{"GET", "POST", "PUT", "DELETE", "OPTIONS"},
+		AllowedHeaders:   []string{"Accept", "Authorization", "Content-Type", "X-CSRF-Token"},
+		ExposedHeaders:   []string{"Link"},
+		AllowCredentials: false,
+		MaxAge:           300, // Maximum value not ignored by any of major browsers
+	}))
 	router.Use(middleware.Logger)
-	router.Use(middleware.Heartbeat("/"))
+	router.Use(middleware.Heartbeat("/ping"))
 	router.Get("/", func(w http.ResponseWriter, r *http.Request) {})
 	router.Route("/auth", func(auth chi.Router) {
 		auth.Post("/register", h.register)
@@ -30,6 +41,7 @@ func (h *Handler) InitRoutes() *chi.Mux {
 		})
 		api.Route("/tag", func(tag chi.Router) {
 			tag.Get("/{tagNames}", h.findByTag)
+			tag.Post("/name", h.TagsByLetters)
 		})
 	})
 	return router
